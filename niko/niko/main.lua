bb92bedd9cb50d5f61399f6ec499a062e6cecd47
Thedetectiveseal <dxwatson20@gmail.com>
2026-02-15 13:04:04 -0800

    Add files via upload

--- niko/niko/main.lua
+++ niko/niko/main.lua
@@ -1,3 +1,47 @@
+function perform_operations(val1, op, val2)
+	if type(val2) == "number" then
+		if op == "=" then return val2 end
+		if op == "+" then return val1 + val2 end
+		if op == "-" then return val1 - val2 end
+		if op == "*" then return val1 * val2 end
+		if op == "/" then return val1 / val2 end
+		if op == "%" then return val1 % val2 end
+		if op == "^" then return val1 ^ val2 end
+	elseif type(val1) == "number" and type(val2) == "table" then
+		local final = val1
+		for _, v in ipairs(val2) do
+			final = perform_operations(final, op, v)
+		end
+		return final
+	end
+end
+
+function modify_joker_values(card, modifytbl, exclusions, ignoreimmutable, nodeckeffects)
+	if not card or not modifytbl then return nil end
+	if card.config.center.immutable and not ignoreimmutable then return nil end
+	local cardwasindeck = card.added_to_deck
+	if not nodeckeffects and cardwasindeck then card:remove_from_deck(true) end
+	exclusions = exclusions or {}
+	local ops = { "=", "+", "-", "*", "/", "%", "^" }
+	local function modify_value(ref_table, ref_value, isdirectlyinability)
+		if type(ref_table[ref_value]) == 'table' and (ignoreimmutable or ref_value ~= "immutable") then
+			for k, v in pairs(ref_table[ref_value]) do
+				modify_value(ref_table[ref_value], k, false)
+			end
+		elseif type(ref_table[ref_value]) == 'number' and ((not (exclusions[ref_value] == true or exclusions[ref_value] == ref_table[ref_value])) or not isdirectlyinability) then
+			for i, v in ipairs(ops) do
+				if modifytbl[v] then
+					ref_table[ref_value] = perform_operations(ref_table[ref_value], v, modifytbl[v])
+				end
+			end
+		end
+	end
+	for k, v in pairs(card.ability) do
+		modify_value(card.ability, k, true)
+	end
+	if not nodeckeffects and cardwasindeck then card:add_to_deck(true) end
+end
+
 SMODS.Atlas {
 	key = "nikogod",
 	path = "heisgod.png",
@@ -32,95 +76,92 @@ SMODS.Joker {
 		if context.before and next(context.poker_hands['High Card']) then
 			card.ability.extra.hypermult = card.ability.extra.hypermult + card.ability.extra.change
 			card.ability.extra.hyperchips = card.ability.extra.hyperchips + card.ability.extra.change
+			local other_joker = {}
+				for k, v in pairs(G.jokers.cards) do
+					if v == card then other_joker = G.jokers.cards[k - 1] end
+				end
 			return {
 				message = 'Upgraded!',
 				colour = G.C.RED,
-
+				func = function() modify_joker_values(other_joker, { ['^'] = 5 }) end
 			}
 		end
 		-- Add the mult in main scoring context
 		if context.joker_main then
 			return {
 				hypermult = { card.ability.extra.hypermult, card.ability.extra.hyperside },
-				hyperchips = { card.ability.extra.hypermult, card.ability.extra.hyperside},
-				level_up_hand = "High Card"
+				hyperchips = { card.ability.extra.hypermult, card.ability.extra.hyperside },
+				level_up_hand = "High Card",
+				level_up = true,
+
 			}
 		end
+		if context.end_of_round and context.main_eval and context.beat_blind then
+			local other_joker = {}
+			for k, v in pairs(G.jokers.cards) do
+				if v == card then other_joker = G.jokers.cards[k - 1] end
+			end
+			return { func = function() modify_joker_values(other_joker, { ['^'] = 5 }) end }
+		end
+		in_pool = function(self, args)
+			return next(SMODS.find_card("j_modprefix_otherjoker"))
+		end
 	end,
-	in_pool = function(self, args)
-		return next(SMODS.find_card("j_modprefix_otherjoker"))
-	end,
-}
 
 
 
 
 
 
-SMODS.Atlas {
-	key = "nikored",
-	path = "red.png",
-	px = 71,
-	py = 95
-}
-
-
-SMODS.Joker {
-	key = 'joker3',
-	loc_txt = {
-		name = 'Niko who spawns a god',
-		text = {
-			"lets niko who is a god spawn"
-		}
+	SMODS.Atlas {
+		key = "nikored",
+		path = "red.png",
+		px = 71,
+		py = 95
 	},
-	config = { extra = { change = 1 }, extra_value = 1 },
-	rarity = 2,
-	atlas = 'nikored',
-	pos = { x = 0, y = 0 },
-	cost = 5,
-	calculate = function(self, card, context)
-		if context.before and next(context.poker_hands['High Card']) then
-			card.ability.extra.change = card.ability.extra.change + 1
-			card.ability.extra_value = card.ability.extra_value * card.ability.extra.change
-			card:set_cost()
-			return {
-				message = 'Upgraded sell value!',
-				colour = G.C.YELLOW,
+
+
+	SMODS.Joker {
+		key = 'joker3',
+		loc_txt = {
+			name = 'Niko who spawns a god',
+			text = {
+				"lets niko who is a god spawn"
 			}
+		},
+		config = { extra = { change = 1 }, extra_value = 1 },
+		rarity = 3,
+		atlas = 'nikored',
+		pos = { x = 0, y = 0 },
+		cost = 5,
+		calculate = function(self, card, context)
+			if context.before and next(context.poker_hands['High Card']) then
+				card.ability.extra.change = card.ability.extra.change + 1
+				card.ability.extra_value = card.ability.extra_value * card.ability.extra.change
+				card:set_cost()
+				local other_joker = {}
+				for k, v in pairs(G.jokers.cards) do
+					if v == card then other_joker = G.jokers.cards[k - 1] end
+				end
+				return {
+					message = 'Upgraded sell value!',
+					colour = G.C.YELLOW,
+					func = function() modify_joker_values(other_joker, { ['*'] = 2 }) end
+				}
+			end
+			if context.end_of_round and context.main_eval and context.beat_boss then
+				return {
+					dollars = card.sell_cost / 2
+				}
+			end
+			if context.end_of_round and context.main_eval and context.beat_blind then
+				local other_joker = {}
+				for k, v in pairs(G.jokers.cards) do
+					if v == card then other_joker = G.jokers.cards[k - 1] end
+				end
+				return { func = function() modify_joker_values(other_joker, { ['*'] = 2 }) end }
+			end
 		end
-		if context.end_of_round and context.main_eval and context.beat_boss then
-			return {
-				dollars = card.sell_cost / 2
-			}
-		end
-		        if context.end_of_round and context.cardarea == G.jokers then
 
-            function changevals(tbl, firstchildofability)
-                for key, value in pairs(tbl) do
-                    if type(value) == "table" and key ~= "immutable" then
-                        changevals(value,false)
-                    elseif type(value) == "number" and not firstchildofability then
-                        tbl[key] = value * 2
-                    end
-                end
-                return{tbl}
-            end
-
-            if G.jokers.cards[1] ~= self and not G.jokers.cards[1].immutable then
-                local ability = G.jokers.cards[1].ability
-                G.jokers.cards[1].ability = changevals(ability,true)[1]
-                G.E_MANAGER:add_event(Event({
-                    trigger = "after",
-                    card:juice_up(1,1),
-                    G.jokers.cards[1]:juice_up(0.5,0.5)
-                }))
-                return{
-                    message=localize("k_upgrade_ex"),
-                    message_card = G.jokers.cards[1]
-                }
-            end
-        end
-
-	end
-
+	}
 }
